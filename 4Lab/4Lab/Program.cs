698fa6558af49510077a729f782e09022c31f4e2
angelinausenko <22fmif.a.usenko@std.npu.edu.ua>
2023-05-22 14:25:00 +0300

    44lab

--- 4Lab/4Lab/Program.cs
+++ 4Lab/4Lab/Program.cs
@@ -1,36 +1,133 @@
-﻿namespace _4Lab;
-class Student
+﻿using System;
+
+namespace EducationlSystem
 {
-    static void Main(string[] args)
+    class Program
     {
-        //Навчальна система
+        static void Main(string[] args)
+        {
+            StudentInformation(); //Виклик методу для збору інформації про студента;
+            TeacherInformation(); //Виклик методу для збору інформації про педагога
+            UniversityInformation(); //Виклик методу для збору інформації про університет;
 
-        Console.WriteLine("Введіть своє прізвище та ім'я:");
-        string studentName = Console.ReadLine();
+            //Закінчити програму
+            Console.WriteLine("Натисніть будь-яку клавішу для завершення роботи.");
+            Console.ReadKey();
+        }
 
-        Console.WriteLine("Введіть свій вік:");
-        int studentAge = Convert.ToInt32(Console.ReadLine());
+        static void StudentInformation()
+        {
+            // Дані студента
 
-        Console.WriteLine("Введіть свій середній бал:");
-        double averageGrade = Convert.ToDouble(Console.ReadLine());
+            // Заголовок
+            Console.WriteLine("Інформація про студента:");
 
-        Console.WriteLine("Напишіть назву навчального закладу:");
-        string universityName = Console.ReadLine();
+            // Дії
+            Console.WriteLine("");
+            Console.WriteLine("Введіть своє прізвище та ім'я:");
+            string studentName = Console.ReadLine();
 
-        Console.WriteLine("Інформація про студента:");
-        Console.WriteLine("Ім'я: " + studentName);
-        Console.WriteLine("Вік: " + studentAge);
-        Console.WriteLine("Середній бал: " + averageGrade);
-        Console.WriteLine("Назва навчального закладу: " + universityName);
+            Console.WriteLine("Введіть свій вік:");
+            int studentAge = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Натисніть будь-яку клавішу для завершення роюоти.");
-        Console.ReadKey();
+            Console.WriteLine("Напишіть назву навчального закладу:");
+            string universityName = Console.ReadLine();
+
+            Console.WriteLine("Введіть назву вашого факультету: ");
+            string facultyName = Console.ReadLine();
+
+            Console.WriteLine("Введіть свій середній бал:");
+            double averageGrade = Convert.ToDouble(Console.ReadLine());
+
+            //Заповнена анкета
+            Console.WriteLine("");
+            Console.WriteLine("Дані студента:");
+            Console.WriteLine("Ім'я: " + studentName);
+            Console.WriteLine("Вік: " + studentAge);
+            Console.WriteLine("Навчальний заклад: " + universityName);
+            Console.WriteLine("Факультет:" + facultyName);
+            Console.WriteLine("Середній бал: " + averageGrade);
+        }
+
+        static void TeacherInformation()
+        {
+            // Дані педагога
+
+            // Заголовок
+            Console.WriteLine("");
+            Console.WriteLine("Інформація про педагога:");
+            Console.WriteLine("");
+
+            // Дії
+            Console.WriteLine("Введіть своє прізвище та ім'я:");
+            string teacherName = Console.ReadLine();
+
+            Console.WriteLine("Введіть свій вік:");
+            int teacherAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Напишіть назву навчального закладу, де працюєте:");
+            string schoolName = Console.ReadLine();
+
+            Console.WriteLine("Введіть предмет, який викладаєте (можна декілька):");
+            string subject = Console.ReadLine();
+
+            //Заповнена анкета
+            Console.WriteLine("");
+            Console.WriteLine("Дані педагога:");
+            Console.WriteLine("Ім'я: " + teacherName);
+            Console.WriteLine("Вік: " + teacherAge);
+            Console.WriteLine("Навчальний заклад: " + schoolName);
+            Console.WriteLine("Предмет:" + subject);
 
 
+        }
 
+        static void UniversityInformation()
+        {
+            //Дані про університет
 
+            // Заголовок
+            Console.WriteLine("");
+            Console.WriteLine("Інформація про університет:");
+            Console.WriteLine("");
 
+            //Дії
+            Console.WriteLine("Введіть назву університету:");
+            string universityName = Console.ReadLine();
+
+            Console.WriteLine("Введіть рік заснування університету:");
+            int yearFounded = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Чи працює університет? (введіть true або false):");
+            bool isWorking = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Скільки студентів навчається в університеті:");
+            int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Скільки вчителів працює в університеті:");
+            int numberOfTeachers = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Чи є університет державним? (введіть true або false):");
+            bool isPublic = Convert.ToBoolean(Console.ReadLine());
+
+            //Заповнена анкета
+            Console.WriteLine("");
+            Console.WriteLine("Дані про університет:");
+            Console.WriteLine("Назва: " + universityName);
+            Console.WriteLine("Рік заснування: " + yearFounded);
+            Console.WriteLine("Чи працює: " + isWorking);
+            Console.WriteLine("Кількість студентів: " + numberOfStudents);
+            Console.WriteLine("Кількість вчителів: " + numberOfTeachers);
+            Console.WriteLine("Державний: " + isPublic);
+        }
     }
 
 
 }
+
+
+       
+
+
+
+       
---
+++ 5Lab/5Lab/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace EducationalSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть своє ім'я:");
+            string studentName = Console.ReadLine();
+
+            Console.WriteLine("Введіть свій вік:");
+            int studentAge = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введіть свій середній бал:");
+            double averageGrade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введіть статус зарахування (True або False):");
+            bool isEnrolled = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Інформація про студента:");
+            Console.WriteLine("Ім'я: " + studentName);
+            Console.WriteLine("Вік: " + studentAge);
+            Console.WriteLine("Середній бал: " + averageGrade);
+            Console.WriteLine("Статус зарахування: " + isEnrolled);
+
+            Console.WriteLine("Статичні дані:");
+            Console.WriteLine("Кількість студентів: " + Student.Count);
+            Console.WriteLine("Максимальний бал: " + Student.MaxGrade);
+
+            Console.WriteLine("Натисніть будь-яку клавішу для завершення...");
+            Console.ReadKey();
+        }
+    }
+
+    class Student
+    {
+        private string name;
+        private int age;
+        private double averageGrade;
+        private bool isEnrolled;
+
+        public static int Count { get; private set; }
+        public static double MaxGrade { get; set; }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string value)
+        {
+            name = value;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        public void SetAge(int value)
+        {
+            age = value;
+        }
+
+        public double GetAverageGrade()
+        {
+            return averageGrade;
+        }
+
+        public void SetAverageGrade(double value)
+        {
+            averageGrade = value;
+        }
+
+        public bool GetIsEnrolled()
+        {
+            return isEnrolled;
+        }
+
+        public void SetIsEnrolled(bool value)
+        {
+            isEnrolled = value;
+        }
+    }
+}
